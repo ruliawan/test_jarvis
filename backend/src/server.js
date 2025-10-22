174b0739377aaeefbcf2d9a7f1fa0602f4d77c5f
ruliawan <58392925+ruliawan@users.noreply.github.com>
2025-10-22 13:57:06 +0700

    deploy server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,25 +3,32 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import cors from "cors";
-
 
 dotenv.config();
-
 const app = express();
+
+
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://68f87d89f48f3fc70f4ce886--jarvis-fe.netlify.app" 
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: true, credentials: true }));
-app.use(cors({
-  origin: ["https://68f87d89f48f3fc70f4ce886--jarvis-fe.netlify.app"],
-  credentials: true,
-}));
-
 app.use("/api", authRoutes);
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

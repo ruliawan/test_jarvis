2c69600db613bba05b542fe3b373cf3ec51abe43
ruliawan <58392925+ruliawan@users.noreply.github.com>
2025-10-22 18:12:50 +0700

    revert again again

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,10 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
+
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://jarvis-fe.netlify.app",
-  "https://68f87d89f48f3fc70f4ce886--jarvis-fe.netlify.app"
+  "http://localhost:5173", 
+  "https://68f87d89f48f3fc70f4ce886--jarvis-fe.netlify.app" 
 ];
 
 app.use(
@@ -23,8 +23,6 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -32,5 +30,5 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
